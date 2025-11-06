c34e7785ed3450e76a18938b3acde7f2808a3a36
rscopim <ricardo.simines@gmail.com>
2025-11-06 09:33:48 -0300

    Exercicios 1

---
+++ Exercicios_1/soma.py
@@ -0,0 +1,9 @@
+'''2- Calculadora de Soma
+* Desenvolva um programa que soma dois números. Use as variáveis numero1 = 12 e numero2 = 14. O programa deve calcular a soma e exibir o resultado.'''
+
+numero1 = 12
+numero2 = 14
+
+resultado = numero1 + numero2
+
+print("A soma de", numero1,"e", numero2, "é: ", resultado)
